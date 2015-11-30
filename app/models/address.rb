4d83e3719a16d0e67cf6bce23e9ff58c867dab9a
Sukwon Choi <swchoi06@wafflestudio.com>
2015-11-30 18:12:04 +0900

    Added Address model validation

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,4 +1,17 @@
 class Address < ActiveRecord::Base
+  # Callbacks 
+  before_save  :remove_whitespace
+
+  # Validation
+  validates :postcode, presence: true
+  validates :postcode, format: { with: /\d\d\d\d\d/,
+    message: "Invalid postcode format" }
+  # Association
   belongs_to :business_license
   belongs_to :station
+
+  private
+  def remove_whitespace(address)
+    address.poastcode = address.postcode.delete(' ').delete('-')
+  end
 end
