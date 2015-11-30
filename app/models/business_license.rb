8708e517f33c8a0048a579435cf7c77f4147fe39
Sukwon Choi <swchoi06@wafflestudio.com>
2015-11-30 18:12:31 +0900

    Added BuinessLicense model validation

--- app/models/business_license.rb
+++ app/models/business_license.rb
@@ -1,4 +1,16 @@
 class BusinessLicense < ActiveRecord::Base
+  # Callbacks
+  before_save :remove_whitespace
+
+  # Validation
+  validates :reg_number, :company_name, :director_name, presence: true
+
+  # Association
   belongs_to :company, polymorphic: true
   has_one :address
+
+  private
+  def remove_whitespace(license)
+    license.reg_number = license.reg_number.delete(' ').delete('-')
+  end
 end
