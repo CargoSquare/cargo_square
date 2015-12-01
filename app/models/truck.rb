caec89d6bffddf2b0fe28eb7e10106ba211f5920
Sukwon Choi <swchoi06@wafflestudio.com>
2015-12-01 09:09:07 +0900

    Added Truck model validation

--- app/models/truck.rb
+++ app/models/truck.rb
@@ -1,3 +1,22 @@
 class Truck < ActiveRecord::Base
+  # Truck Category
+  @categories = ["일반", "윙", "탑"]
+  # Callbacks
+  before_save :remove_useless_character
+  # Validation
+  validates :number, :category, :size, presence: true
+  validates :category, inclusion: {in: @categories }
+  # Association
   belongs_to :driver, class_name: "TruckDriver", foreign_key: "truck_driver_id"
+
+  private
+  def remove_useless_character
+    truck = self
+    if truck.number != nil
+      truck.number = truck.number.gsub(/[\s]/, '')
+    end
+    if truck.category != nil
+      truck.category = truck.category.gsub(/[\s]/, '')
+    end
+  end
 end
