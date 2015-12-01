668024d0ced9b6487aa5f57133a68d64d0c68d02
Sukwon Choi <swchoi06@wafflestudio.com>
2015-12-01 09:13:35 +0900

    Added TruckDriver model validation

--- app/models/truck_driver.rb
+++ app/models/truck_driver.rb
@@ -1,4 +1,21 @@
 class TruckDriver < ActiveRecord::Base
+  # Callbacks
+  before_save :remove_useless_character
+
+  # Validation
+  validates :name, presence: true
+  # Association
   belongs_to :company, class_name: "TruckingCompany", foreign_key: "trucking_company_id"
   has_one :truck
+
+  private
+  def remove_useless_character
+    driver = self
+    if driver.phone_number != nil
+      driver.phone_number.gsub!(/[^0-9\s]/, '')
+    end
+    if driver.email != nil
+      driver.email.gsub!(/[\s]/, '')
+    end
+  end
 end
