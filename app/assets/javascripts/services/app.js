e15a958c538e8f22d5304291e79e9b1661ba675c
Seok jun Seo <seokjunseo@Seokui-MacBook-Pro.local>
2015-12-09 16:19:04 +0900

    user token auth integrate done

--- app/assets/javascripts/services/app.js
+++ app/assets/javascripts/services/app.js
@@ -1,1 +1,32 @@
 angular.module('app.services', [])
+
+.factory('loginService', ['$auth', '$state', function ($auth, $state) {
+	return function (query) {
+		$auth.submitLogin(query)
+			.then(function (resp) {
+				console.log("login service: successfully login!");
+				$state.go('welcome');
+			})
+			.catch(function (resp) {
+				// TODO: Error handling
+				console.log("login service: Error => " + JSON.stringfy(resp));
+			})
+	}
+}])
+
+.factory('logoutService', ['$auth', '$state', function ($auth, $state) {
+	return function () {
+		$auth.signOut()
+		.then(function (resp) {
+			// move to home
+			console.log("logout service: successfully logout!");
+			$state.go('welcome');
+		})
+		.catch(function (resp) {
+			// TODO: Error handling
+			console.log("logout service: Error => " + JSON.stringfy(resp));
+		});
+	}
+}])
+;
+
